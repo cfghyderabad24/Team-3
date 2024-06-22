10376d146d3ec440012666e6079f8c05f86f60bd
Suhaani Batra <suhaanibatra23@gmail.com>
2024-06-22 18:01:20 +0530

    changing directory structure

--- client/components/readingLevelProgress.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function readingLevelProgress() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
---
+++ client/src/components/ReadingLevelProgress.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReadingLevelProgress() {
+  return (
+    <div>
+      ReadingLevelProgress
+    </div>
+  )
+}
